ec79cad040a17040b34bc39ed5c6bbf533e73085
RKhatoon19 <rubina.khatoon.7587@gmail.com>
2024-01-11 15:14:22 +0530

    courses reducers

---
+++ src/app/courses/courses.reducer.ts
@@ -0,0 +1,17 @@
+import { EntityState, createEntityAdapter } from "@ngrx/entity";
+import { Course } from "./model/course";
+import { createReducer, on } from "@ngrx/store";
+import { ActionCouses } from "./courses-type";
+
+export interface CoursesState extends EntityState<Course>{
+
+}
+export const adapter= createEntityAdapter<Course>();
+export const initialState = adapter.getInitialState();
+export const courseReducer = createReducer(
+    initialState,
+    on(ActionCouses.allCoursesLoaded,
+    (state,action)=>
+        adapter.addMany(action.courses,state)
+    )
+)
